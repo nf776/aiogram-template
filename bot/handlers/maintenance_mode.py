c7982b16ccd4ad115288c73cb760f27cba801cb2
nf776 <crq1c1@gmail.com>
2025-03-29 15:13:41 +0300

    refactoring: maintenance mode handler

--- bot/handlers/maintenance_mode.py
+++ bot/handlers/maintenance_mode.py
@@ -10,26 +10,20 @@ router = Router()
 router.message.filter(MagicData(F.maintenance_mode.is_(True)))
 router.callback_query.filter(MagicData(F.maintenance_mode.is_(True)))
 
+l10n = FluentLocalization
+msg = l10n.format_value(
+    msg_id='maintenance-id',
+    args={
+        'asctime': datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
+        'supportbot': config.support_bot
+    }
+)
+
+
 @router.message()
-async def any_message_in_m_mode(message: Message, l10n: FluentLocalization):
-    await message.answer(
-        l10n.format_value(
-            msg_id='maintenance-id',
-            args={
-                'asctime': datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
-                'supportbot': config.support_bot
-            }
-        )
-    )
+async def any_message_in_m_mode(message: Message):
+    await message.answer(text=msg)
 
 @router.callback_query()
-async def any_callback_in_m_mode(callback: CallbackQuery, l10n: FluentLocalization):
-    await callback.message.answer(
-        l10n.format_value(
-            msg_id='maintenance-id',
-            args={
-                'asctime': datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
-                'supportid': config.support_id
-            }
-        )
-    )
+async def any_callback_in_m_mode(callback: CallbackQuery):
+    await callback.message.answer(text=msg)
